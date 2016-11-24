28faf733eb0aca188720ecbdd403807a35c8e7b1
Lukasz Dabrowski <alf_1990@tlen.pl>
2016-11-24 21:05:58 +0100

    sdas

--- BMI Calculator/BMI Calculator/Program.cs
+++ BMI Calculator/BMI Calculator/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BMI_Calculator
 {
@@ -12,55 +8,65 @@ namespace BMI_Calculator
         {
             double waga, wzrost, bmi, bmi2;
 
-
-            Console.WriteLine("podaj swoj wzrost (w cm): ");
-            wzrost = double.Parse(Console.ReadLine());
-            Console.WriteLine("podaj swoja wage (w kg): ");
-            waga = double.Parse(Console.ReadLine());
-
-
-            bmi2 = (waga / (wzrost * wzrost)*10000);
-
-            bmi = Math.Round(bmi2, 2);
-
-
-            Console.WriteLine("Twoje BMI to: {0} \n", bmi);
-
-
-            if (bmi <= 16.00)
+            while (true)
             {
-                Console.WriteLine("Twoj organizm jest wyglodzony !\n");
-            }
-            else if (bmi > 16.00 && bmi <= 17.00) 
-            {
-                Console.WriteLine("Jestes wychudzony\n");
+
+                Console.WriteLine("podaj swoj wzrost (w cm): ");
+                wzrost = double.Parse(Console.ReadLine());
+                Console.WriteLine("podaj swoja wage (w kg): ");
+                waga = double.Parse(Console.ReadLine());
+
+
+                bmi2 = (waga / (wzrost * wzrost) * 10000);
+
+                bmi = Math.Round(bmi2, 2);
+
+
+                Console.WriteLine("Twoje BMI to: {0} \n", bmi);
+
+
+                if (bmi <= 16.00)
+                {
+                    Console.WriteLine("Twoj organizm jest wyglodzony !!!\n");
+                }
+                else if (bmi > 16.00 && bmi <= 17.00)
+                {
+                    Console.WriteLine("Jestes wychudzony\n");
+
+                }
+                else if (bmi > 17.00 && bmi <= 18.50)
+                {
+                    Console.WriteLine("Masz niedowage\n");
+                }
+                else if (bmi > 18.50 && bmi <= 25.00)
+                {
+                    Console.WriteLine("Twoja waga jest prawidlowa, GRATULACJE !\n");
+                }
+                else if (bmi > 25.00 && bmi <= 30.00)
+                {
+                    Console.WriteLine("Masz nadwage\n");
+                }
+                else if (bmi > 30.00 && bmi <= 35.00)
+                {
+                    Console.WriteLine("Masz pierwszy stopien otylosci\n");
+                }
+                else if (bmi > 35.00 && bmi <= 40.00)
+                {
+                    Console.WriteLine("Masz drugi stopien otylosci\n");
+                }
+                else
+                {
+                    Console.WriteLine("Masz otylosc skrajna !!!\n");
+                }
+                //Console.ReadLine();
+
+                Console.WriteLine("Obliczyć bmi ponownie ? T/N");
+
+                if (Console.ReadLine() == "N")
+                    break;
+
 
             }
-            else if (bmi > 17.00 && bmi <= 18.50)
-            {
-                Console.WriteLine("Masz niedowage\n");
-            }
-            else if (bmi > 18.50 && bmi <= 25.00)
-            {
-                Console.WriteLine("Twoja waga jest prawidlowa, GRATULACJE !\n");
-            }
-            else if (bmi > 25.00 && bmi <= 30.00)
-            {
-                Console.WriteLine("Masz nadwage\n");
-            }
-            else if (bmi > 30.00 && bmi <= 35.00)
-            {
-                Console.WriteLine("Masz pierwszy stopien otylosci\n");
-            }
-            else if (bmi > 35.00 && bmi <= 40.00)
-            {
-                Console.WriteLine("Masz drugi stopien otylosci\n");
-            }
-            else
-            {
-                Console.WriteLine("Masz otylosc skrajna !!!\n");
-            }
-            Console.ReadLine();
         }
     }
 }
---
+++ Doktor Czyste Zlo/Doktor Czyste Zlo/Doktor Czyste Zlo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doktor_Samo_Zlo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tarcza;
+
+            Random kulaOgnia = new Random();
+
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("{0}. Bajtek powinienes osłaniac nasze plecy ! \nUwazaj żeby Doktor Czyste Zlo nas nie zaatakowal !", i);
+                Console.ReadLine();
+
+
+                if (kulaOgnia.Next(0, 10) < 5)
+                {
+                    Console.WriteLine("UWAZAJ! Leci kula ognia! Czyste Zlo atakuje!");
+
+                    tarcza = "Woda";
+
+                    Console.WriteLine("Tarcza: {0}", tarcza);
+                    Console.ReadLine();
+
+                }
+                else
+                {
+                    Console.WriteLine("Uff, cale szczescie Doktor Czyste Zlo nas nie zaatakowal.");
+
+                    tarcza = "Brak tarczy";
+
+                    Console.WriteLine("Tarcza: {0}", tarcza);
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
